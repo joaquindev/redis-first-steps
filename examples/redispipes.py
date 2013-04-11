7513b963bab6035d58897a442acc6d8257bdc941
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-11 14:49:28 +0100

    Added the pipelining example

---
+++ examples/redispipes.py
@@ -0,0 +1,26 @@
+import time
+
+import redis
+
+TIMES = 1000000
+
+
+def with_pipelining():
+    r = redis.Redis()
+    p = r.pipeline()
+    map(p.incr, range(TIMES))
+    p.execute()
+
+
+def without_pipelining():
+    r = redis.Redis()
+    map(r.incr, range(TIMES))
+
+
+start = time.clock()
+without_pipelining()
+print("Without pipelining: {0} seconds".format(time.clock() - start))
+
+start = time.clock()
+with_pipelining()
+print("With pipelining: {0} seconds".format(time.clock() - start))
