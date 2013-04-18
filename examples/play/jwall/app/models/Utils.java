5027abb9cc7be50d54d1e78fb76ad71717de8a3d
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-18 14:52:40 +0100

    Added Heroku redis url checker

--- examples/play/jwall/app/models/Utils.java
+++ examples/play/jwall/app/models/Utils.java
@@ -3,6 +3,9 @@ package models;
 import play.Play;
 import redis.clients.jedis.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: slok
@@ -18,8 +21,24 @@ public class Utils {
     static public JedisPool getRedisPool(){
         if (pool == null){
             //TODO: Fix the test so they start a fake application when the test starts (needed to take the conf)
-            //String host = Play.application().configuration().getString("redis.host");
-            Utils.pool = new JedisPool(new JedisPoolConfig(), "localhost");
+            try {
+                String env = System.getenv("ENVIRONMENT");
+                if (env != null && env.equals("heroku")){
+                URI redisURI = new URI(System.getenv("REDISCLOUD_URL"));
+
+                Utils.pool = new JedisPool(new JedisPoolConfig(),
+                        redisURI.getHost(),
+                        redisURI.getPort(),
+                        Protocol.DEFAULT_TIMEOUT,
+                        redisURI.getUserInfo().split(":",2)[1]);
+                }else{
+                    String host = Play.application().configuration().getString("redis.host");
+                    Utils.pool = new JedisPool(new JedisPoolConfig(), host);
+                }
+            } catch (URISyntaxException e) {
+                // URI couldn't be parsed. Handle exception
+            }
+
 
         }
         return Utils.pool;
